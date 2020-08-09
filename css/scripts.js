7d226261fe04924e4243ecc4ee18aaece332b17f
sonali <leopanthara9@gmail.com>
2020-08-09 23:12:43 +0530

    my first commit

---
+++ css/scripts.js
@@ -0,0 +1,35 @@
+
+function myFunction()
+{
+ location.replace("login.html")
+}
+
+
+function myFun()
+{
+ location.replace("sign.html")
+}
+function Home()
+{
+ location.replace("index.html")
+}
+function Fruits()
+{
+ location.replace("fruit.html")
+}
+function Vegetables()
+{
+ location.replace("Veg.html")
+}
+function Non()
+{
+ location.replace("nonveg.html")
+}
+function More()
+{
+ location.replace("More.html")
+}
+function About()
+{
+ location.replace("About.html")
+}
